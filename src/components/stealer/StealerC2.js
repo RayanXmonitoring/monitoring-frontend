3d64e4f2bd0a9d92c064b15183624629dd9bdb12
RayanXmonitoring <evaanjelinaaswitalumbanggaol@gmail.com>
2026-06-17 01:15:59 +0700

    Create StealerC2.js

---
+++ src/components/stealer/StealerC2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StealerC2 = () => {
+  return (
+    <div className="space-y-4">
+      <h2 className="text-xl font-bold text-white">Stealer C2</h2>
+      <div className="bg-gray-800 rounded-xl border border-gray-700 p-6">
+        <p className="text-gray-400">Halaman Stealer C2</p>
+      </div>
+    </div>
+  );
+};
+
+export default StealerC2;
