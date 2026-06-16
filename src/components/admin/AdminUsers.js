cbfcd8c58d4e37b9d0be399187fb24de15050a9d
RayanXmonitoring <evaanjelinaaswitalumbanggaol@gmail.com>
2026-06-17 01:08:58 +0700

    Create AdminUsers.js

---
+++ src/components/admin/AdminUsers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AdminUsers = () => {
+  return (
+    <div className="space-y-4">
+      <h2 className="text-xl font-bold text-white">Kelola User</h2>
+      <div className="bg-gray-800 rounded-xl border border-gray-700 p-6">
+        <p className="text-gray-400">Halaman kelola user</p>
+      </div>
+    </div>
+  );
+};
+
+export default AdminUsers;
